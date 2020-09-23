d825e773d00cf598c11a8aeccbdd5fd37274db1a
Art3m198 <klass125@yandex.ru>
2020-09-23 16:54:59 +0300

    Create database.sql

---
+++ database.sql
@@ -0,0 +1,79 @@
+-- phpMyAdmin SQL Dump
+-- version 3.5.1
+-- http://www.phpmyadmin.net
+--
+-- Хост: 127.0.0.1
+-- Время создания: Сен 23 2020 г., 13:54
+-- Версия сервера: 5.5.25
+-- Версия PHP: 5.6.19
+
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- База данных: `report`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `reports`
+--
+
+CREATE TABLE IF NOT EXISTS `reports` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `idreport` int(11) unsigned DEFAULT NULL,
+  `lastname` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `firstname` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `group` int(11) unsigned DEFAULT NULL,
+  `date` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `name` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `disc` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `file` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `estimation` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `signature` varchar(191) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci AUTO_INCREMENT=3 ;
+
+--
+-- Дамп данных таблицы `reports`
+--
+
+INSERT INTO `reports` (`id`, `idreport`, `lastname`, `firstname`, `group`, `date`, `name`, `disc`, `file`, `estimation`, `signature`) VALUES
+(1, 7, 'Adam', 'Jensen', 4652, '23.09.2020 16:21', 'Homework', 'History', 'upload/7/1600867314.jpg', 'A', '');
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `right` int(11) NOT NULL,
+  `lastname` varchar(50) DEFAULT NULL,
+  `firstname` varchar(50) DEFAULT NULL,
+  `group` longtext,
+  `email` longtext,
+  `password` longtext,
+  `photo` varchar(191) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=9 ;
+
+--
+-- Дамп данных таблицы `users`
+--
+
+INSERT INTO `users` (`id`, `right`, `lastname`, `firstname`, `group`, `email`, `password`, `photo`) VALUES
+(7, 0, 'Adam', 'Jensen', '4652', 'adam@gmail.com', '$2y$10$nBs3610AfGWTZHfDmbi4yO6sHH3Oq0XBStJnEG2enrhWw3MJOBSHu', 'upload/profile/7/7.png'),
+(8, 1, 'Admin', '#1', '-', 'admin@gmail.com', '$2y$10$hU5OltlIfVlNeCLDQI8fN.TNEV/f.pF9rZuASV2WuwnJKkUjbmD1C', 'upload/profile/8/8.png');
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
